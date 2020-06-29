e9191d8403dfadea175412417a90c7980eeb61c0
Brandon Everett <bme2010@gmail.com>
2020-06-29 18:28:41 -0400

    add counter for useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Message from './Message';
 
-function message(props: any) {
-	return <div>{props.text}</div>;
+function Counter() {
+	const [count, setCount] = useState(0);
+
+	const counterClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<label>The count is {count}</label>
+			<br />
+			<button onClick={counterClick}>Count</button>
+		</div>
+	);
 }
 
 const App: React.FC = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<Message text="Hello world" />
-				<Message text="Goodbye world" />
+				<Counter />
 			</header>
 		</div>
 	);
