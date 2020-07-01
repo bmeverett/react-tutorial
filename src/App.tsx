2ac90263771aa2126cf7ab7bceb568681ec13ba6
Brandon Everett <bme2010@gmail.com>
2020-07-01 10:03:51 -0400

    add useEffect example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Counter() {
+	const [name, setName] = useState('');
 	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		console.log('useEffect called');
+		document.title = name;
+	});
+
+	const nameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setName(event.currentTarget.value);
+	};
 
 	const counterClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 		setCount(count + 1);
@@ -10,6 +20,10 @@ function Counter() {
 
 	return (
 		<div>
+			<label>What is your name</label>
+			<br />
+			<input onChange={nameChange} />
+			<br />
 			<label>The count is {count}</label>
 			<br />
 			<button onClick={counterClick}>Count</button>
